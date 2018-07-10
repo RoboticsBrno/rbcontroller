d489db9a8f4c8978fd52738f32182ed4ae2f68e9
Vojtech Bocek <vbocek@gmail.com>
2018-07-10 12:08:07 +0200

    Fix AsciiInputFilter, release 0.7

--- app/src/main/java/com/tassadar/rbcontroller/AsciiInputFilter.kt
+++ app/src/main/java/com/tassadar/rbcontroller/AsciiInputFilter.kt
@@ -2,16 +2,32 @@ package com.tassadar.rbcontroller
 
 import android.text.InputFilter
 import android.text.Spanned
+import android.text.TextUtils
+import android.text.SpannableString
+
 
 class AsciiInputFilter : InputFilter {
-    override fun filter(source: CharSequence?, start: Int, end: Int, dest: Spanned?, dstart: Int, dend: Int): CharSequence {
-        val sb = StringBuilder()
-        for(i in start until end) {
+    override fun filter(source: CharSequence?, start: Int, end: Int, dest: Spanned?, dstart: Int, dend: Int): CharSequence? {
+        var keepOriginal = true
+        val sb = StringBuilder(end - start)
+        for (i in start until end) {
             val c = source!![i].toInt()
-            if(c in 1..127) {
-                sb.append(source[i])
+            if (c in 1..127)
+                sb.append(c)
+            else
+                keepOriginal = false
+        }
+
+        if (keepOriginal)
+            return null;
+        else {
+            if (source is Spanned) {
+                val sp = SpannableString(sb)
+                TextUtils.copySpansFrom(source, start, end, null, sp, 0)
+                return sp
+            } else {
+                return sb
             }
         }
-        return sb.toString()
     }
 }
